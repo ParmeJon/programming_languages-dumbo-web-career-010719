e2b0d02708918128918b870dbb55966eefc9c4c4
Jonathan Chan <jonchan816@gmail.com>
2019-01-07 01:00:16 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,14 @@
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  style = {}
+  
+  languages.each do |type_of, language|
+    unless new_hash.key?(language) 
+      new_hash[language] = {:type => language[:type], :style => [languages[type_of]]}
+    end
+  end
+  
+  puts new_hash
 end
+
